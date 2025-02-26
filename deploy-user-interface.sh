548c155e9bed1339eb35551bd62f91fdf4c9f44a
Dan Villarreal <d.vill@pitt.edu>
2025-02-26 09:51:11 -0500

    Deployment script: Permit spaces in paths

--- deploy-user-interface.sh
+++ deploy-user-interface.sh
@@ -5,21 +5,22 @@ LOCAL_LABBCAT=${1:-/var/lib/tomcat9/webapps/labbcat}
 
 if (mvn clean package -pl :nzilbb.labbcat.user-interface)
 then
-    if [ ! -d $LOCAL_LABBCAT/user-interface ]; then
-        mkdir $LOCAL_LABBCAT/user-interface;
+    if [ ! -d "$LOCAL_LABBCAT/user-interface" ]; then
+        mkdir "$LOCAL_LABBCAT/user-interface";
     fi
-    rm -rf $LOCAL_LABBCAT/user-interface/*
-    cp -r user-interface/target/labbcat-view/browser/* $LOCAL_LABBCAT/user-interface/
+    rm -rf "$LOCAL_LABBCAT/user-interface/*"
+    cp -r user-interface/target/labbcat-view/browser/* "$LOCAL_LABBCAT/user-interface/"
     
-    if [ ! -d $LOCAL_LABBCAT/edit/user-interface ]; then
-        mkdir -p $LOCAL_LABBCAT/edit/user-interface;
+    if [ ! -d "$LOCAL_LABBCAT/edit/user-interface" ]; then
+        mkdir -p "$LOCAL_LABBCAT/edit/user-interface";
     fi
-    rm -rf $LOCAL_LABBCAT/edit/user-interface/*
-    cp -r user-interface/target/labbcat-edit/browser/* $LOCAL_LABBCAT/edit/user-interface/
+    rm -rf "$LOCAL_LABBCAT/edit/user-interface/*"
+    cp -r user-interface/target/labbcat-edit/browser/* "$LOCAL_LABBCAT/edit/user-interface/"
     
-    if [ ! -d $LOCAL_LABBCAT/admin/user-interface ]; then
-        mkdir -p $LOCAL_LABBCAT/admin/user-interface;
+    if [ ! -d "$LOCAL_LABBCAT/admin/user-interface" ]; then
+        mkdir -p "$LOCAL_LABBCAT/admin/user-interface";
     fi
-    rm -rf $LOCAL_LABBCAT/admin/user-interface/*
-    cp -r user-interface/target/labbcat-admin/browser/* $LOCAL_LABBCAT/admin/user-interface/
+    rm -rf "$LOCAL_LABBCAT/admin/user-interface/*"
+    cp -r user-interface/target/labbcat-admin/browser/* "$LOCAL_LABBCAT/admin/user-interface/"
 fi
+

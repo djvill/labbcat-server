d23521725cbad3439688f694e8a1132a4b704e09
Dan Villarreal <d.vill@pitt.edu>
2025-02-27 11:10:40 -0500

    Development: Add quick-deploy script for labbcat-view

---
+++ user-interface/src/main/angular/deploy-view.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Script for building the client-side **view** files,
+# and deploying them to a local instance of LaBB-CAT,
+# adding "prev-" prefix to existing index.html
+
+LOCAL_LABBCAT=${1:-"/c/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/apls-dev"}
+LL_UI="$LOCAL_LABBCAT/user-interface/en"
+
+ng build labbcat-common --configuration production
+ng build labbcat-view --configuration production
+
+mv "$LL_UI/index.html" "$LL_UI/prev-index.html"
+cd dist/labbcat-view/browser
+cp -t "$LL_UI" index.html *js *css

8255459717ce19bdf1c0447415576e5391ac590c
Dan Villarreal <d.vill@pitt.edu>
2025-03-21 14:41:25 -0400

    Development: If deploy-view fails, don't overwrite LaBB-CAT

--- user-interface/src/main/angular/deploy-view.sh
+++ user-interface/src/main/angular/deploy-view.sh
@@ -4,13 +4,15 @@
 # and deploying them to a local instance of LaBB-CAT,
 # adding "prev-" prefix to existing index.html
 
-LOCAL_LABBCAT=${1:-"/c/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/apls-dev"}
+LOCAL_LABBCAT=${1:-"/c/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/labbcat"}
 LL_UI="$LOCAL_LABBCAT/user-interface/en"
 
-ng build labbcat-common --configuration production
-ng build labbcat-view --configuration production
-
-mv "$LL_UI/index.html" "$LL_UI/prev-index.html"
-cd dist/labbcat-view/browser
-cp -t "$LL_UI" index.html *js *css
-cp media/* "$LL_UI/media"
+if (
+  ng build labbcat-common --configuration production && #comment-out if not rebuilding
+  ng build labbcat-view --configuration production
+  ) ; then
+  mv "$LL_UI/index.html" "$LL_UI/prev-index.html"
+  cd dist/labbcat-view/browser
+  cp -t "$LL_UI" index.html *js *css
+  cp media/* "$LL_UI/media"
+fi

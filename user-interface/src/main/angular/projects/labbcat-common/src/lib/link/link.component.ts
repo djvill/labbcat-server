djvill/labbcat-server

commit 652eb5e724fd7db0f972dfcbbccd4ba115e36fe3

layer-checkboxes: Add optional explainer links to controls

user-interface/src/main/angular/projects/labbcat-common/src/lib/link/link.component.ts
@@ -14,6 +14,7 @@ export class LinkComponent implements OnInit {
     @Input() routerLink: string[];
     @Input() queryParams: object;
     @Input() href: string;
+    @Input() target: string;
     processing: false;
     
     imagesLocation: string;

user-interface/src/main/angular/projects/labbcat-common/src/lib/layer-checkboxes/layer-checkboxes.component.css
@@ -80,13 +80,26 @@ img.data-type, img.alignment, img.vertical-peers {
 }
 #controls {
     font-style: italic;
+    display: flex;
+    flex-direction: column;
 }
 @media all and (min-width: 640px) {
     #controls { 
-        display: flex;
         flex-direction: row;
+        align-items: flex-end;
     }
     #controls label {
         flex-grow: 1;
     }
+    #controls #about-link {
+        flex-grow: 0;
+        margin-left: auto;
+    }
+}
+#controls .control-link {
+    font-style: normal;
+    font-size: 0.85em;
+}
+#controls #about-link {
+    font-style: normal;
 }

user-interface/src/main/angular/projects/labbcat-common/src/lib/layer-checkboxes/layer-checkboxes.component.html
@@ -412,7 +412,7 @@
   </ng-container>
 </fieldset>
 </div>
-<fieldset *ngIf="!hideControls && (includeDataType || includeAlignment || includeVerticalPeers || (interpretedRaw | json) != '{}' || (annotationCounts | json) != '{}' || participant || transcript)" id="controls">
+<fieldset *ngIf="!hideControls && (includeDataType || includeAlignment || includeVerticalPeers || (interpretedRaw | json) != '{}' || (annotationCounts | json) != '{}' || participant || transcript || controlsLinks['about'])" id="controls">
   <label 
     *ngIf="includeDataType || includeAlignment || includeVerticalPeers || (interpretedRaw | json) != '{}'"
     title="Hide icons that correspond to layer properties">
@@ -424,6 +424,9 @@
       (change)="toggleLayerIcons()"
       ></lib-grouped-checkbox>
     Hide layer icons
+    <span *ngIf="controlsLinks['layerIcons']"
+          class="control-link">(<a [href]="controlsLinks['layerIcons']" target="_blank">about</a>)
+    </span>
   </label>
   <label 
     *ngIf="(annotationCounts | json) != '{}'"
@@ -435,6 +438,9 @@
       (change)="toggleLayerCounts()"
       ></lib-grouped-checkbox>
     Hide annotation counts
+    <span *ngIf="controlsLinks['layerCounts']"
+          class="control-link">(<a [href]="controlsLinks['layerCounts']" target="_blank">about</a>)
+    </span>
   </label>
   <label 
     *ngIf="participant || transcript"
@@ -446,5 +452,16 @@
       (change)="toggleAttributePrefixes()"
       ></lib-grouped-checkbox>
     Show attribute prefixes
+    <span *ngIf="controlsLinks['attributePrefixes']"
+          class="control-link">(<a [href]="controlsLinks['attributePrefixes']" target="_blank">about</a>)
+    </span>
   </label>
+  <lib-link
+    *ngIf="controlsLinks['about']"
+    id="about-link"
+    [href]="controlsLinks['about']['href']"
+    [label]="controlsLinks['about']['text'] || 'About'"
+    img="howto.svg"
+    target="_blank"
+    icon="🎓"></lib-link>
 </fieldset>

user-interface/src/main/angular/projects/labbcat-common/src/lib/layer-checkboxes/layer-checkboxes.component.ts
@@ -83,6 +83,9 @@ export class LayerCheckboxesComponent implements OnInit {
     @Input() interpretedRaw: { [key: string] : any };
     /** Output list of layers with interpreted (true) or raw (false) labels */
     @Output() interpretedRawChange = new EventEmitter<{ [key: string] : any }>();
+    /** Links for controls bar - key is the control name (e.g. layerIcons), value is the URL.
+        key = 'about' for the right-hand help, with value {text: (link text), href: (URL) }*/
+    @Input() controlsLinks: { [key: string] : any };
     /** Hide controls bar, overriding the effect of any other settings that would normally trigger showing it */
     @Input() hideControls = false;
     
@@ -121,6 +124,7 @@ export class LayerCheckboxesComponent implements OnInit {
         if (!this.disabled) this.disabled = [];
         if (!this.preselected) this.preselected = [];
         if (!this.interpretedRaw) this.interpretedRaw = {};
+        if (!this.controlsLinks) this.controlsLinks = {};
     }
 
     loadSchema(): void {

user-interface/src/main/angular/projects/labbcat-common/src/lib/link/link.component.html
@@ -12,6 +12,7 @@
 </a>
 <a *ngIf="href"
    href="{{href}}"
+   [target]="target"
    title="{{title}}"
    class="{{classes}}">
   <img *ngIf="img"

user-interface/src/main/angular/projects/labbcat-view/src/app/matches/matches.component.html
@@ -166,6 +166,7 @@
           span="true" phrase="true" word="true" segment="true"
           excludeMainParticipant="true" excludeTurn="true" excludeUtterance="true"
           includeAnchorSharing="true" excludeOptionsForLayerId="{{task.targetLayer}}"
+          [controlsLinks]="controlsLinksCSV"
           selected="{{selectedLayers}}"></lib-layer-checkboxes>
       </div>
     </fieldset>
@@ -233,6 +234,7 @@
     <legend i18n>Layers to export</legend>
       <lib-layer-checkboxes name="layerId"
                             word="true" segment="true" phrase="true" excludeTurn="true"
+                            [controlsLinks]="controlsLinksSerialization"
                             selected="{{serializationLayers}}"></lib-layer-checkboxes>
     </fieldset>
   </div>

user-interface/src/main/angular/projects/labbcat-view/src/app/matches/matches.component.ts
@@ -64,6 +64,19 @@ export class MatchesComponent implements OnInit {
     annotationLayerId: string;
     phoneAlignmentLayerId: string;
     
+    controlsLinksCSV = {
+        about: {
+            text: 'About layers and attributes',
+            href: 'https://djvill.github.io/APLS/doc/layers-and-attributes'
+        }
+    };
+    controlsLinksSerialization = {
+        about: {
+            text: 'About layers',
+            href: 'https://djvill.github.io/APLS/doc/layers-and-attributes'
+        }
+    };
+    
     constructor(
         private labbcatService: LabbcatService,
         private messageService: MessageService,

user-interface/src/main/angular/projects/labbcat-view/src/app/search-matrix/search-matrix.component.html
@@ -6,6 +6,7 @@
   category="true"
   [(selected)]="selectedLayerIds"
   (selectedChange)="syncSelectedLayerIdsWithColumns($event)"
+  [controlsLinks]="controlsLinks"
   ></lib-layer-checkboxes>
 
 <div class="columns">

commit b7a373c001f3e49bcc499bc11e6c917ecbb0d6b8

link (minor): Fix 'target=undefined' bug introduced in 652eb5e

user-interface/src/main/angular/projects/labbcat-common/src/lib/link/link.component.ts
@@ -27,6 +27,7 @@ export class LinkComponent implements OnInit {
     ngOnInit(): void {
         this.title = this.title || this.label;
         if (!this.label) this.classes += " icon-only";
+        this.target = this.target || "";
     }
     
 }
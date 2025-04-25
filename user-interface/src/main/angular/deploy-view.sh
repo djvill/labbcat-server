4dbb9520d12c007d03eae8af5195fcbce355714a
Dan Villarreal <d.vill@pitt.edu>
2025-04-25 09:44:44 -0400

    Development: Add -c/--common switch to deploy-view.sh

--- user-interface/src/main/angular/deploy-view.sh
+++ user-interface/src/main/angular/deploy-view.sh
@@ -4,15 +4,31 @@
 # and deploying them to a local instance of LaBB-CAT,
 # adding "prev-" prefix to existing index.html
 
-LOCAL_LABBCAT=${1:-"/c/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/apls-dev"}
+LOCAL_LABBCAT="/c/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/apls-dev"
 LL_UI="$LOCAL_LABBCAT/user-interface/en"
 
+common=${1}
+if [ -z $common ] ; then
+  build_common=false
+elif [ $common = "-c" ] || [ $common = "--common" ] ; then
+  build_common=true
+else
+  echo Option can only be -c or --common
+  echo "Syntax: bash deploy-view.sh [-c|--common]"
+fi
+
 if (
-  ng build labbcat-common --configuration production && #comment-out if not rebuilding
-  ng build labbcat-view --configuration production
+  if $build_common ; then
+    ng build labbcat-common --configuration production &&
+    ng build labbcat-view --configuration production
+  else
+    ng build labbcat-view --configuration production
+  fi
   ) ; then
   mv "$LL_UI/index.html" "$LL_UI/prev-index.html"
   cd dist/labbcat-view/browser
   cp -t "$LL_UI" index.html *js *css
   cp media/* "$LL_UI/media"
+  echo Built at $(date)
 fi
+

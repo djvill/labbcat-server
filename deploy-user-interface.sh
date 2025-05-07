fae1eaa44f5ab23b1e806eedc6b739d95f03113e
Dan Villarreal <d.vill@pitt.edu>
2025-05-07 12:13:58 -0400

    Deployment: Change icon colors to Pitt gold at deploy-time
    This way, the diff between this branch and its upstream remote (i.e.,
    https://github.com/nzilbb/labbcat-server/compare/main...djvill:labbcat-server:apls-dev)
    doesn't have to be clogged with SVGs

--- deploy-user-interface.sh
+++ deploy-user-interface.sh
@@ -10,17 +10,26 @@ then
     fi
     rm -rf "$LOCAL_LABBCAT/user-interface/*"
     cp -r user-interface/target/labbcat-view/browser/* "$LOCAL_LABBCAT/user-interface/"
+    for svg in "$LOCAL_LABBCAT"/user-interface/*/assets/*svg ; do 
+        sed -i 's/#859044/#FFB81C/g' "$svg"
+    done
     
     if [ ! -d "$LOCAL_LABBCAT/edit/user-interface" ]; then
         mkdir -p "$LOCAL_LABBCAT/edit/user-interface";
     fi
     rm -rf "$LOCAL_LABBCAT/edit/user-interface/*"
     cp -r user-interface/target/labbcat-edit/browser/* "$LOCAL_LABBCAT/edit/user-interface/"
+    for svg in "$LOCAL_LABBCAT"/edit/user-interface/*/assets/*svg ; do 
+        sed -i 's/#859044/#FFB81C/g' "$svg"
+    done
     
     if [ ! -d "$LOCAL_LABBCAT/admin/user-interface" ]; then
         mkdir -p "$LOCAL_LABBCAT/admin/user-interface";
     fi
     rm -rf "$LOCAL_LABBCAT/admin/user-interface/*"
     cp -r user-interface/target/labbcat-admin/browser/* "$LOCAL_LABBCAT/admin/user-interface/"
+    for svg in "$LOCAL_LABBCAT"/admin/user-interface/*/assets/*svg ; do 
+        sed -i 's/#859044/#FFB81C/g' "$svg"
+    done
 fi
 

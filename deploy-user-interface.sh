ee868132f92666831a92c9d5e3e2bcabee085c1d
Dan Villarreal <d.vill@pitt.edu>
2025-10-13 15:58:18 -0400

    Deployment: Add header help icon (don't change color)

--- deploy-user-interface.sh
+++ deploy-user-interface.sh
@@ -10,8 +10,10 @@ then
     fi
     rm -rf "$LOCAL_LABBCAT/user-interface/*"
     cp -r user-interface/target/labbcat-view/browser/* "$LOCAL_LABBCAT/user-interface/"
-    for svg in "$LOCAL_LABBCAT"/user-interface/*/*/*svg ; do 
-        sed -i 's/#859044/#FFB81C/g' "$svg"
+    for svg in "$LOCAL_LABBCAT"/user-interface/*/*/*svg ; do
+	if [[ "$svg" != *help.svg ]] ; then
+            sed -i 's/#859044/#FFB81C/g' "$svg";
+        fi
     done
     
     if [ ! -d "$LOCAL_LABBCAT/edit/user-interface" ]; then

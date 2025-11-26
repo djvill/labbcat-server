7cf381f1cc4aee46a6314237c8b07ce5dac66ae6
Dan Villarreal <d.vill@pitt.edu>
2025-11-26 17:36:05 -0500

    layer-checkboxes: Remember interpretedRaw settings in sessionStorage

--- user-interface/src/main/angular/projects/labbcat-common/src/lib/layer-checkboxes/layer-checkboxes.component.ts
+++ user-interface/src/main/angular/projects/labbcat-common/src/lib/layer-checkboxes/layer-checkboxes.component.ts
@@ -118,7 +118,6 @@ export class LayerCheckboxesComponent implements OnInit {
         if (!this.disabled) this.disabled = [];
         if (!this.excludeOptionsForLayerId) this.excludeOptionsForLayerId = [];
         if (!this.annotationCounts) this.annotationCounts = {};
-        if (!this.interpretedRaw) this.interpretedRaw = {};
     }
 
     loadSchema(): void {
@@ -144,6 +143,9 @@ export class LayerCheckboxesComponent implements OnInit {
         this.displayCounts = JSON.parse(sessionStorage.getItem("displayLayerCounts")) ??
             (typeof this.displayCounts == "string" ? this.displayCounts === "true" : this.displayCounts) ??
             true;
+        this.interpretedRaw = JSON.parse(sessionStorage.getItem("interpretedLabels")) ??
+            this.interpretedRaw ??
+            {};
         if (!this.selected) this.selected = [] as string[];
 
         // add category selectors in defined order
@@ -275,6 +277,7 @@ export class LayerCheckboxesComponent implements OnInit {
         if (!this.disabled || !this.disabled.includes(layerId)) {
             this.interpretedRaw[layerId] = !this.interpretedRaw[layerId];
             this.interpretedRawChange.emit(this.interpretedRaw);
+            sessionStorage.setItem("interpretedLabels", JSON.stringify(this.interpretedRaw));
         }
     }
     
